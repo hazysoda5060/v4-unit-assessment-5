1298a37b9569dfcaba8bff3357a9409f0b9c1b16
alex <alexanderstapp@gmail.com>
2021-05-14 10:12:50 -0600

    user auth setup

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -15,4 +15,29 @@ module.exports = {
         req.session.user = user
         return res.status(200).send(req.session.user)
     },
+    login: async (req, res) => {
+        const db = req.app.get('db')
+        const {username, password} = req.body
+        const [user] = await db.auth.find_user_by_username(username)
+        if (!user) {
+            return res.status(401).send('user not registerd')
+        }
+        const isAuthenticated = bcrypt.compareSync(password, user.password)
+        if(!isAuthenticated) {
+            return res.status(403).send('password incorrect')
+        }
+        delete user.password
+        req.session.user = user
+        return res.status(200).send(req.session.user)
+    },
+    logout: (req, res) => {
+        req.session.destroy()
+        return res.status(200)
+    },
+    getUser: (req, res) => {
+        if(!req.session.user) {
+            return res.status(404).send('user not found')
+        }
+        return res.status(200).send(req.session.user)
+    }
 }
